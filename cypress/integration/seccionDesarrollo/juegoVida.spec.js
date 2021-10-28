9b2dd573d83feba536b19538bc40b3a00b0d3cad
manuvikn <manuelvictoriacode@gmail.com>
2021-10-28 18:11:47 +0200

    implementacion test juego de la vida

--- cypress/integration/seccionDesarrollo/juegoVida.spec.js
+++ cypress/integration/seccionDesarrollo/juegoVida.spec.js
@@ -1,5 +1,5 @@
-const { Panel } = require('../../models/panel');
-const { generarPanelVisual } = require('../../models/utils/functions');
+import { Panel } from '../../models/panel';
+import {generarPanelVisual, compararPaneles, comprobarClaseCeldas, clickCelda} from '../../models/utils/functions';
 
 require('cypress-xpath');
 
@@ -11,6 +11,10 @@ describe('Banco de test para la aplicación "Juego de la vida"', function() {
         cy.visit('http://localhost:4200');
 
     });
+
+    after(() => { // Hook after all
+        cy.log('Fin del flujo funcional de la aplicación');
+    })
 
 
     it('Verificación de titulo y que no existan partidas', () => {
@@ -46,41 +50,82 @@ describe('Banco de test para la aplicación "Juego de la vida"', function() {
         cy.get('@partida').children().eq(2).children().then(botonera => {
             expect(botonera.length).to.equal(4);
         });
+
+        cy.log('Todos los elementos visuales se encuentran presentes');
         
     });
 
     it('Prueba de funcionalidad botón "Siguiente turno" pt1', () => {
         
+        // Se genera una matriz a partir del panel visual mostrado en el navegador
         const tablero = generarPanelVisual();
         const panel = new Panel(tablero);
 
         cy.xpath("//button[contains(text(),'Siguiente turno')]").should('be.visible')
         .and('have.text', 'Siguiente turno').as('next');
 
-        cy.get('@next').click();
+        // Se hace un "Siguiente turno" tanto en el componente visual como en el lógico
+        cy.get('@next').click(); // Siguiente turno en el componente visual
+        panel.nextTurn().then(() => this.panel = panel); // Siguiente turno en el componente lógico
 
-        panel.nextTurn().then(() => this.panel = panel);
+        // Al haber hecho un siguiente turno la generación debe de haber pasado de 0 a 1
+        cy.xpath('//p[contains(text(),"Generación")]').then(element => {
+            const num = Number(element.text().split(': ')[1]);
+            expect(num).to.equal(1);
+        });
 
     });
 
     it('Prueba de funcionalidad botón "Siguiente turno" pt2', () => {
         
+        // Se comparan ambos paneles para comprobar que la lógica funcione correctamente
         const tablero = generarPanelVisual();
-        console.log(tablero);
-        console.log('************************************************');
-        console.log(this.panel);
+
+        compararPaneles(tablero, this.panel.panel).then((iguales) => {
+            expect(iguales).to.equal(true);
+        });
+        
+        cy.log('Prueba funcionalidad botón "Siguiente turno" finalizada con éxito');
         
     });
 
-    /* it('Generación de panel visual extraido del panel lógico (Angular)', () => {
+    it('Prueba de funcionalidad botón "Cuadrícula"', () => {
         
-        this.panel = new Panel(generarPanelVisual());
+        cy.xpath('//body/app-root[1]/app-main[1]/div[1]/app-partida[1]/div[1]/div[3]/button[3]')
+        .should('be.visible').as('cuadricula');
 
-        cy.xpath("//button[contains(text(),'Siguiente turno')]").should('be.visible')
-        .and('have.text', 'Siguiente turno').as('next');
+        // Se comprueba que al iniciar partida las celdas no tengan cuadrícula
+        // Al pulsar el botón se comprueba que esta vez si contengan esa cuadrícula
+        comprobarClaseCeldas(0).then(claseCelda => {
+            expect(claseCelda).to.equal(true);
+            cy.get('@cuadricula').click();
+            comprobarClaseCeldas(1).then(claseConBorde => {
+                expect(claseConBorde).to.equal(true);
+                cy.get('@cuadricula').click();
+            });
+        });
 
-        cy.get('@next').click();
+        cy.log('Prueba funcionalidad botón "Cuadrícula" finalizada con éxito');
 
-    }); */
+    });
+
+    it('Prueba de funcionalidad al pulsar en una de las celdas', () => {
+
+        // Se comprueba que al pulsar celdas al azar su background-color cambia
+        clickCelda();
+
+    });
+
+    it('Prueba de funcionalidad botón "Borrar partida"', () => {
+        
+        // Se comprueba que al pulsar el boton borrar partida, esta se elimine
+        cy.xpath('//body/app-root[1]/app-main[1]/div[1]/app-partida[1]/div[1]/div[3]/button[4]')
+        .should('be.visible').as('borrar').click();
+
+        cy.xpath('//body/app-root[1]/app-main[1]/div[1]/div[1]')
+        .children().eq(0).should('have.text', 'No tienes ninguna partida');
+
+    });
+    
 
 });
--- cypress/models/utils/functions.js
+++ cypress/models/utils/functions.js
@@ -1,4 +1,3 @@
-
 export function generarPanelVisual() {
     
     cy.xpath('//body/app-root[1]/app-main[1]/div[1]/app-partida[1]/div[1]/div[1]').as('panel')
@@ -21,4 +20,75 @@ export function generarPanelVisual() {
     });
     
     return panel;
+}
+
+export function comprobarClaseCeldas(opt) {
+
+    return new Promise((resolve) => {
+        const clases = [ 'celda', 'celda conBorde']
+        
+        cy.xpath('//body/app-root[1]/app-main[1]/div[1]/app-partida[1]/div[1]/div[1]').as('panel')
+        .should('be.visible');
+        
+        cy.get('@panel').children().each(fila => {
+        
+            const columnsArr = fila.children();
+            
+            for (let i = 0; i < columnsArr.length; i++) {
+                if (columnsArr[i].className !== clases[opt]) {
+                    resolve(false);
+                }
+            }
+            resolve(true);
+        
+        });
+
+    });
+
+
+}
+
+export function clickCelda() {
+
+    for (let i = 0; i < 5; i++) {
+        
+        let fila = Math.round(Math.random() * 49);
+        let col = Math.round(Math.random() * 159);
+        let colorAfter;
+        let colorBefore;
+    
+        cy.xpath('//body/app-root[1]/app-main[1]/div[1]/app-partida[1]/div[1]/div[1]')
+        .should('be.visible').children().eq(fila).as('fila');
+    
+        cy.get('@fila').children().eq(col).then(elAfter => {
+            colorAfter = elAfter.css('background-color');
+        }).click().then(() => {
+            cy.get('@fila').children().eq(col).then(elBefore => {
+                colorBefore = elBefore.css('background-color');
+                
+                expect(colorAfter).to.not.equal(colorBefore);
+    
+            });
+        });
+
+    }
+
+
+}
+
+export const compararPaneles = (tablero, panel) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            for (let i = 0; i < tablero.length; i++) {
+                for (let j = 0; j < tablero[i].length; j++) {
+                    if (tablero[i][j] !== panel[i][j]) {
+                        resolve(false);
+                    }       
+                }
+            }
+            resolve(true);
+        });
+
+    });
+
 }
